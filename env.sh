9f0636bb4e7c62ff2962d489d123ab39030ed5d6
Rodrigo Ceccato <rodrigoceccatodefreitas@gmail.com>
2024-08-29 00:40:28 -0300

    add ompc experiment

--- env.sh
+++ env.sh
@@ -1,4 +1,5 @@
 module load lammps/20240207.1-gcc-12.2.0-vha7pxa
 module load gromacs/2023-gcc-12.2.0-zktp5lx
 module load python/3.11.7
+module load singularity/3.7.1
 source .venv/bin/activate
--- steps.sh
+++ steps.sh
@@ -1,3 +1,6 @@
 virtualenv .venv
 source .venv/bin/activate
 pip3 install git+https://github.com/LSC-Unicamp/spinner
+
+singularity pull docker://docker.io/ompcluster/runtime-dev
+mv runtime-dev_latest.sif ./ompc-tb
